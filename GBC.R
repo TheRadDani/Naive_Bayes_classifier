569ae0e9bc04fe56af29c22d0a66b8869760f191
TheRadDani <ldanielfch@gmail.com>
2023-04-21 23:15:57 -0600

    final changes to plot confusion matrix

--- GBC.R
+++ GBC.R
@@ -10,3 +10,24 @@ test$Species <- as.factor(test$Species)
 
 nb_mod <- NaiveBayes(Species ~ ., data=training)
 pred <- predict(nb_mod, test)
+
+
+# Confusion Matrix
+tab <- table(pred$class, test$Species)
+caret::confusionMatrix(tab)  
+
+# Plot density of each feature using nb_mod
+opar = par(mfrow=c(2, 2), mar=c(4,0,0,0))
+plot(nb_mod, main="")  
+par(opar)
+
+# Plot the Confusion Matrix
+library(ggplot2)
+test$pred <- pred$class
+ggplot(test, aes(Species, pred, color = Species)) +
+  geom_jitter(width = 0.2, height = 0.1, size=2) +
+  labs(title="Confusion Matrix", 
+       subtitle="Predicted vs. Observed from Iris dataset", 
+       y="Predicted", 
+       x="Truth",
+       caption="Naive Bayes Clasifier")
